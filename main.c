8ef560121361628c7bf3336b38b4cc14b2ad770e
赵金浩 <2481406659@qq.com>
2019-01-15 19:09:35 +0800

    建立基本工程框架

--- main.c
+++ main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+char arr[2000];
+int index = 0;
+int main()
+{
 
-int main(){
+    printf("minganzifuguanlixitong");
 
-    printf("hello world!");
+    while (1)
+    {
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu\n");
+        printf("3---chakansuoyouminganzifu\n");
+        printf("4---tihuanneirongzhongdeminganzifu\n");
+        printf("5---tuichu\n");
+
+        printf("qingxuanzexiangyinggongneng\n");
+
+        int code;
+        scanf("%d",&code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("tuichuxitong\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
